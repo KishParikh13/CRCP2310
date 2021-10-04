5447f256cf880976c89cd1b53c4b1a2c04915637
KishParikh13 <kaparikh@mail.smu.edu>
2021-10-04 08:19:19 -0500

    saving progress

---
+++ WaveAnimation/Leader.js
@@ -0,0 +1,15 @@
+class Leader {
+
+    constructor(pos) {
+        this.size = createVector(70, 20);
+        this.pos = pos;
+    }
+
+    draw() {
+
+    	this.pos.x = mouseX - width / 2;
+    	this.pos.y = mouseY - height / 2;
+  
+        // image(img, this.pos.x - 55, this.pos.y  - 55, 100, 100);
+    }
+}
